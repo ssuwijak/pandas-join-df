cfe2d8a1cbfbab382dd95e3363fff71d723a7387
ssuwijak <ssuwijak@hotmail.com>
2024-08-18 09:11:23 +0700

    update comment in main.py

--- src/main.py
+++ src/main.py
@@ -4,11 +4,11 @@ import pandas as pd
 df1 = pd.read_csv("file1.txt", header=None)  # Replace with your first file path
 df2 = pd.read_csv("file2.txt", header=None)  # Replace with your second file path
 
-# Assign custom column names (replace with appropriate names)
+# Assign the column indexes to e joined
 df1_index = 0
 df2_index = 2
 
-# Assuming the ID column is named 'id' in both files
+# Join both data frame
 merged_df = pd.merge(df1, df2, how='inner', left_on=df1_index, right_on=df2_index, suffixes=('_file1','_file2'))
 
 # Get columns to be output
